adf973318429a3a538897bac95b2f7cee1c52bcc
vagrant <vagrant@homestead>
2020-12-31 03:11:00 +0000

    3.5. 完善前台商品列表页

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
+use App\Models\Category;
 use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Http\Request;
-// 5.4. 商品列表页面 新建
+// 5.4-old. 商品列表页面 新建
 class ProductsController extends Controller
 {
     /**
@@ -14,11 +15,11 @@ class ProductsController extends Controller
      */
     public function index(Request $request){
         //$products = Product::query()->where('on_sale', true)->paginate();
-        // 5.4. 同章修改
+        // 5.4-old. 同章修改
         /*$products = Product::query()->where('on_sale', true)->paginate(16);
         return view('products.index', ['products' => $products]);*/
 
-        //5.5. 商品筛选、排序 修改
+        //5.5-old. 商品筛选、排序 修改
         // 创建一个查询构造器
         $builder = Product::query()->where('on_sale', true);
         // 判断是否有提交 search 参数，如果有就赋值给 $search 变量
@@ -48,18 +49,34 @@ class ProductsController extends Controller
                 }
             }
         }
+
+        // 3.5. 完善前台商品列表页 添加:如果有传入 category_id 字段，并且在数据库中有对应的类目
+        if ($request->input('category_id') && $category = Category::query()->find($request->input('category_id'))) {
+            // 如果这是一个父类目
+            if ($category->is_directory) {
+                // 则筛选出该父类目下所有子类目的商品
+                $builder->whereHas('category', function ($query) use ($category) {
+                    // 这里的逻辑参考本章第一节
+                    $query->where('path', 'like', $category->path.$category->id.'-%');
+                });
+            } else {
+                // 如果这不是一个父类目，则直接筛选此类目下的商品
+                $builder->where('category_id', $category->id);
+            }
+        }
         $products = $builder->paginate(16);
         //return view('products.index', ['products' => $products]);
         return view('products.index', [
             'products' => $products,
+            'category' => $category ?? null,  // 3.5 添加: 等价于 isset($category) ? $category : null
             'filters'  => [
-                'search' => $search,
-                'order'  => $order,
+                'search'   => $search,
+                'order'    => $order
             ],
         ]);
     }
     /**
-     * 5.6. 商品详情页 添加:
+     * 5.6-old. 商品详情页 添加:
      */
     public function show(Product $product, Request $request){
         // 判断商品是否已经上架，如果没有上架则抛出异常。
@@ -68,7 +85,7 @@ class ProductsController extends Controller
         }
         return view('products.show', ['product' => $product]);*/
 
-        // 5.7 修改
+        // 5.7-old 修改
         if (!$product->on_sale) {
             throw new InvalidRequestException('商品未上架');
         }
@@ -79,7 +96,7 @@ class ProductsController extends Controller
             // boolval() 函数用于把值转为布尔值
             $favored = boolval($user->favoriteProducts()->find($product->id));
         }
-        //8.5 获取商品评价数据 添加:
+        //8.5-old 获取商品评价数据 添加:
         $reviews = OrderItem::query()
             ->with(['order.user', 'productSku']) // 预先加载关联关系
             ->where('product_id', $product->id)
@@ -89,7 +106,7 @@ class ProductsController extends Controller
             ->get();
 
         //return view('products.show', ['product' => $product, 'favored' => $favored]);
-        //8.5 修改
+        //8.5-old 修改
         return view('products.show', [
             'product' => $product,
             'favored' => $favored,
@@ -98,7 +115,7 @@ class ProductsController extends Controller
 
     }
     /**
-     * 5.7. 收藏商品 添加:
+     * 5.7-old. 收藏商品 添加:
      * 1.这段代码先是判断当前用户是否已经收藏了此商品，如果已经收藏则不做任何操作直接返回，否则通过 attach() 方法将当前用户和此商品关联起来;
      * 2.attach() 方法的参数可以是模型的 id，也可以是模型对象本身，因此这里还可以写成 attach($product->id).
      */
@@ -111,7 +128,7 @@ class ProductsController extends Controller
         return [];
     }
     /**
-     * 5.7. 取消收藏商品:
+     * 5.7-old. 取消收藏商品:
      * detach() 方法用于取消多对多的关联，接受的参数个数与 attach() 方法一致
      */
     public function disfavor(Product $product, Request $request){
@@ -120,7 +137,7 @@ class ProductsController extends Controller
         return [];
     }
 
-    //5.7 收藏商品列表
+    //5.7-old 收藏商品列表
     public function favorites(Request $request){
         //这里我们用分页的方式取出当前用户的收藏商品，由于我们在定义关联关系的时候就已经加上了排序规则，这里就不需要再次设置了.
         $products = $request->user()->favoriteProducts()->paginate(16);
